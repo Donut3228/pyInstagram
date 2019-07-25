9b2b8f18304db3df11cd9c192aafda7811b71a15
Dmitriy Ponomar <ppoonncchhiikk@gmail.com>
2019-07-25 11:15:04 +0300

    custom_config and formatting

---
+++ custom_config.py
@@ -0,0 +1,2 @@
+API_LINKS = 'http://167.71.75.165/api/links/'
+API_CODES = 'http://167.71.75.165/api/code/'
--- myscript.py
+++ myscript.py
@@ -1,12 +1,20 @@
 import pprint
 import time
 import os
-from instagram import Account, WebAgentAccount, CheckpointException, InternetException, UnexpectedResponse
+from instagram import (
+    Account,
+    WebAgentAccount,
+    CheckpointException,
+    InternetException,
+    UnexpectedResponse,
+)
 from config import USERNAME, PASSWORD, INSTA_ACC
+from custom_config import API_CODES, API_LINKS
 import requests
 
 first_profile = True
-fp_name = ''
+fp_name = ""
+
 
 def fl():
     global fp_name
@@ -14,15 +22,24 @@ def fl():
     for i in ac[0]:
         if i == fp_name:
             first_profile = False
-        if fp_name == '':
+        if fp_name == "":
             fp_name = i
-        a = Account(i) 
-        agent.update(a) 
-        bio = a.biography 
-        if 'CEO' in bio or 'Founder' in bio or 'founder' in bio or 'owner' in bio or 'Owner' in bio: 
-            print(f'https://www.instagram.com/{a}/') 
-    ab = agent.get_followers(account=base_acc, pointer=ac[1], count=5, limit=2, delay=5) 
+        a = Account(i)
+        agent.update(a)
+        bio = a.biography
+        if (
+            "CEO" in bio
+            or "Founder" in bio
+            or "founder" in bio
+            or "owner" in bio
+            or "Owner" in bio
+        ):
+            link = f"https://www.instagram.com/{a}/"
+            requests.post(API_LINKS, data={"link": link})
+            print(link)
+    ab = agent.get_followers(account=base_acc, pointer=ac[1], count=5, limit=2, delay=5)
     return ab
+
 
 login = Account(USERNAME)
 pw = PASSWORD
@@ -31,8 +48,8 @@ settings = {}
 
 agent = WebAgentAccount(login)
 
-agent_url = ''
-agent_code = ''
+agent_url = ""
+agent_code = ""
 auth_failed = True
 
 
@@ -42,11 +59,16 @@ except CheckpointException as e:
     is_fail = True
     agent_url = e.checkpoint_url
     ch_handle = agent.checkpoint_handle(e.checkpoint_url, settings=settings)
-    agent.checkpoint_send(checkpoint_url=e.checkpoint_url, forward_url=ch_handle['navigation']['forward'], choice = min([x['value'] for x in e.types ]), settings=settings)
+    agent.checkpoint_send(
+        checkpoint_url=e.checkpoint_url,
+        forward_url=ch_handle["navigation"]["forward"],
+        choice=min([x["value"] for x in e.types]),
+        settings=settings,
+    )
     while is_fail:
         try:
-            api_req = requests.get('http://167.71.14.94:8018/c/')
-            agent_code = api_req.json()[-1]['value']
+            api_req = requests.get(API_CODES)
+            agent_code = api_req.json()[-1]["value"]
             print(agent_code)
             agent.checkpoint(agent_url, agent_code)
 
@@ -55,23 +77,7 @@ except CheckpointException as e:
             time.sleep(30)
             continue
 
-    
-# while auth_failed:
-#     try:
-#         print(agent_code)
-#         time.sleep(5)
-#         agent.auth(pw)
-#         time.sleep(2)
-#         auth_failed = False
-#     except Exception as e:
-
-    
-
-
-
 ac = agent.get_followers(base_acc)
-
-
 
 
 while first_profile is True:
@@ -80,4 +86,3 @@ while first_profile is True:
     except InternetException:
         time.sleep(3)
         ac = fl()
-
